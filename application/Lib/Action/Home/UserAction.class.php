ff82fcf5825db112fde271da8721415b4133895a
Yuntao Jiang <jiangyuntao@gmail.com>
2012-11-25 19:57:03 +0800

    首页定稿

--- application/Lib/Action/Home/UserAction.class.php
+++ application/Lib/Action/Home/UserAction.class.php
@@ -5,13 +5,31 @@ class UserAction extends AppAction {
 
     public function signup() {
         if ($this->isPost()) {
-            dump($_POST);
-            exit;
+            $user = D('User');
+            if ($user->create() && $user->add()) {
+            } else {
+            }
         }
         $this->display();
     }
 
     public function signin() {
+        if ($this->isPost()) {
+            $user = D('User');
+
+            if (strpos($_POST['username'], '@')) {
+                $condition = "email='{$_POST['email']}'";
+            } else {
+                $condition = "username='{$_POST['username']}'";
+            }
+
+            if (!$entry = $user->where($condition)->find()) {
+            }
+
+            $if ($entry->password != $user->encryptPassword($_POST['password'])) {
+            }
+        } else {
+        }
         $this->display();
     }
 
