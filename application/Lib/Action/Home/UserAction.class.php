8a7af323940c8e1f103f9642daca65756f65e8be
Yuntao Jiang <jiangyuntao@gmail.com>
2012-11-26 17:26:57 +0800

    修改用户功能

--- application/Lib/Action/Home/UserAction.class.php
+++ application/Lib/Action/Home/UserAction.class.php
@@ -6,10 +6,20 @@ class UserAction extends AppAction {
     public function signup() {
         if ($this->isPost()) {
             $user = D('User');
+            $user->password = $user->encryptPassword($_POST['password']);
             if ($user->create() && $user->add()) {
+                // @todo 成功后自动登录
+                // @todo 成功后跳转到上传头像, UserAction::signup_finish()
             } else {
+                // @todo 跳转到错误页
             }
+            return;
         }
+        $this->display();
+    }
+
+    public function signupFinish() {
+        echo 'foo';
         $this->display();
     }
 
@@ -18,23 +28,45 @@ class UserAction extends AppAction {
             $user = D('User');
 
             if (strpos($_POST['username'], '@')) {
-                $condition = "email='{$_POST['email']}'";
+                $condition = "email='{$_POST['username']}'";
             } else {
                 $condition = "username='{$_POST['username']}'";
             }
 
             if (!$entry = $user->where($condition)->find()) {
+                // @todo 帐号不存在
             }
 
-            $if ($entry->password != $user->encryptPassword($_POST['password'])) {
+            if ($entry->password != $user->encryptPassword($_POST['password'])) {
+                // @todo 密码错误
             }
-        } else {
+
+            $auth = array(
+                'id' => $entry->id,
+                'username' => $entry->username,
+                'avatar' => $entry->avatar,
+            );
+
+            if ($_POST['remember']) {
+                Cookie::set();
+            } else {
+                Cookie::set();
+            }
+
+            return;
         }
+
         $this->display();
+    }
+
+    public function signout() {
     }
 
     public function captcha() {
         import('ORG.Util.Image');
         Image::buildImageVerify();
+    }
+
+    public function avatar_upload() {
     }
 }
--- application/Lib/Model/UserModel.class.php
+++ application/Lib/Model/UserModel.class.php
@@ -1,9 +1,13 @@
 <?php
 class UserModel extends AppModel {
-    protected $_auto = array_merge(parent::$_auto, array(
-        array('signup_time', 'time', Model::MODEL_INSERT, 'function'),
-        array('signup_ip', 'get_client_ip', Model::MODEL_INSERT, 'function'),
-    ));
+    public function __construct() {
+        parent::__construct();
+
+        $this->_auto = array_merge($this->_auto, array(
+            array('signup_time', 'time', Model::MODEL_INSERT, 'function'),
+            array('signup_ip', 'get_client_ip', Model::MODEL_INSERT, 'function'),
+        ));
+    }
 
     public function encryptPassword($password = '') {
         import('ORG.Crypt.Crypt');
