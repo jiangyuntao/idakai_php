75067a2006813e6baecf2e5d7044b22c6e9cddf4
Yuntao Jiang <jiangyuntao@gmail.com>
2012-12-12 17:34:06 +0800

    数据库添加表

--- misc/idakai.sql
+++ misc/idakai.sql
@@ -1,63 +1,45 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- 主机: localhost
--- 生成日期: 2012 年 11 月 15 日 14:21
--- 服务器版本: 5.5.8
--- PHP 版本: 5.3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- 数据库: `idakai`
---
-
+-- --------------------------------------------------------
+-- 主机                            :localhost
+-- Server version                :5.5.16 - MySQL Community Server (GPL)
+-- Server OS                     :Win32
+-- HeidiSQL 版本                   :7.0.0.4246
+-- Created                       :2012-12-12 17:31:39
 -- --------------------------------------------------------
 
---
--- 表的结构 `admin`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for table idakai.admin
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `user_id` int(10) unsigned NOT NULL,
   KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `admin`
---
+-- Dumping data for table idakai.admin: 0 rows
+DELETE FROM `admin`;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `badword`
---
-
+-- Dumping structure for table idakai.badword
+DROP TABLE IF EXISTS `badword`;
 CREATE TABLE IF NOT EXISTS `badword` (
   `word` varchar(255) NOT NULL,
   `replace` varchar(255) NOT NULL,
   PRIMARY KEY (`word`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `badword`
---
+-- Dumping data for table idakai.badword: 0 rows
+DELETE FROM `badword`;
+/*!40000 ALTER TABLE `badword` DISABLE KEYS */;
+/*!40000 ALTER TABLE `badword` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `category`
---
-
+-- Dumping structure for table idakai.category
+DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
@@ -65,19 +47,16 @@ CREATE TABLE IF NOT EXISTS `category` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `category`
---
+-- Dumping data for table idakai.category: 0 rows
+DELETE FROM `category`;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `group`
---
-
+-- Dumping structure for table idakai.group
+DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` int(10) unsigned NOT NULL,
@@ -93,19 +72,16 @@ CREATE TABLE IF NOT EXISTS `group` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `group`
---
+-- Dumping data for table idakai.group: 0 rows
+DELETE FROM `group`;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `message`
---
-
+-- Dumping structure for table idakai.message
+DROP TABLE IF EXISTS `message`;
 CREATE TABLE IF NOT EXISTS `message` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `from_user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '0代表系统消息',
@@ -116,19 +92,16 @@ CREATE TABLE IF NOT EXISTS `message` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `message`
---
+-- Dumping data for table idakai.message: 0 rows
+DELETE FROM `message`;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `moderator`
---
-
+-- Dumping structure for table idakai.moderator
+DROP TABLE IF EXISTS `moderator`;
 CREATE TABLE IF NOT EXISTS `moderator` (
   `user_id` int(10) unsigned NOT NULL,
   `group_id` int(10) unsigned NOT NULL,
@@ -136,17 +109,36 @@ CREATE TABLE IF NOT EXISTS `moderator` (
   KEY `group_id` (`group_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `moderator`
---
+-- Dumping data for table idakai.moderator: 0 rows
+DELETE FROM `moderator`;
+/*!40000 ALTER TABLE `moderator` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moderator` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
+-- Dumping structure for table idakai.p
+DROP TABLE IF EXISTS `p`;
+CREATE TABLE IF NOT EXISTS `p` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `alias` varchar(255) NOT NULL,
+  `sortorder` int(10) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `keywords` varchar(255) NOT NULL,
+  `decription` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `created` int(10) unsigned NOT NULL,
+  `modified` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `alias` (`alias`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 表的结构 `post`
---
+-- Dumping data for table idakai.p: 0 rows
+DELETE FROM `p`;
+/*!40000 ALTER TABLE `p` DISABLE KEYS */;
+/*!40000 ALTER TABLE `p` ENABLE KEYS */;
 
+
+-- Dumping structure for table idakai.post
+DROP TABLE IF EXISTS `post`;
 CREATE TABLE IF NOT EXISTS `post` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `topic_id` int(10) unsigned NOT NULL,
@@ -157,19 +149,16 @@ CREATE TABLE IF NOT EXISTS `post` (
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `topic_id` (`topic_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `post`
---
+-- Dumping data for table idakai.post: 0 rows
+DELETE FROM `post`;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `setting`
---
-
+-- Dumping structure for table idakai.setting
+DROP TABLE IF EXISTS `setting`;
 CREATE TABLE IF NOT EXISTS `setting` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `variable` varchar(255) NOT NULL,
@@ -177,36 +166,30 @@ CREATE TABLE IF NOT EXISTS `setting` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `setting`
---
+-- Dumping data for table idakai.setting: 0 rows
+DELETE FROM `setting`;
+/*!40000 ALTER TABLE `setting` DISABLE KEYS */;
+/*!40000 ALTER TABLE `setting` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `subscription`
---
-
+-- Dumping structure for table idakai.subscription
+DROP TABLE IF EXISTS `subscription`;
 CREATE TABLE IF NOT EXISTS `subscription` (
   `topic_id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
   KEY `user_id` (`user_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `subscription`
---
+-- Dumping data for table idakai.subscription: 0 rows
+DELETE FROM `subscription`;
+/*!40000 ALTER TABLE `subscription` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subscription` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `topic`
---
-
+-- Dumping structure for table idakai.topic
+DROP TABLE IF EXISTS `topic`;
 CREATE TABLE IF NOT EXISTS `topic` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `category_id` int(10) unsigned NOT NULL,
@@ -219,19 +202,16 @@ CREATE TABLE IF NOT EXISTS `topic` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `topic`
---
+-- Dumping data for table idakai.topic: 0 rows
+DELETE FROM `topic`;
+/*!40000 ALTER TABLE `topic` DISABLE KEYS */;
+/*!40000 ALTER TABLE `topic` ENABLE KEYS */;
 
 
--- --------------------------------------------------------
-
---
--- 表的结构 `user`
---
-
+-- Dumping structure for table idakai.user
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
@@ -239,9 +219,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(255) NOT NULL,
   `show_email` enum('0','1') NOT NULL DEFAULT '0',
   `avatar` varchar(255) NOT NULL,
-  `reg_ip` varchar(15) NOT NULL,
-  `login_time` int(10) unsigned NOT NULL,
-  `login_ip` varchar(15) NOT NULL,
+  `signup_time` int(10) unsigned NOT NULL,
+  `signup_ip` varchar(15) NOT NULL,
+  `signin_time` int(10) unsigned NOT NULL,
+  `signin_ip` varchar(15) NOT NULL,
   `banned` enum('0','1') NOT NULL DEFAULT '0',
   `banned_time` int(10) unsigned NOT NULL,
   `banned_reason` varchar(255) NOT NULL,
@@ -249,11 +230,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created` int(10) unsigned NOT NULL,
   `modified` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
---
--- 转存表中的数据 `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `show_email`, `avatar`, `reg_ip`, `login_time`, `login_ip`, `banned`, `banned_time`, `banned_reason`, `posts`, `created`, `modified`) VALUES
-(1, 'admin', '9b15d22deb8b650ee73934ccb938f2a0', '', '0', '', '127.0.0.1', 1332333587, '127.0.0.1', '0', 0, '', 0, 0, 0);
+-- Dumping data for table idakai.user: 1 rows
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `show_email`, `avatar`, `signup_time`, `signup_ip`, `signin_time`, `signin_ip`, `banned`, `banned_time`, `banned_reason`, `posts`, `created`, `modified`) VALUES
+	(1, 'admin', 'b316a59c785db342b22c871cfdba0357', 'admin@idakai.com', '0', '', 1354093079, '127.0.0.1', 0, '', '0', 0, '', 0, 1354093079, 1354093079);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
