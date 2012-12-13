cec81f4bce51872188fb09459c9a3d3f75a56d86
Yuntao Jiang <jiangyuntao@gmail.com>
2012-12-13 23:03:22 +0800

    后台小组分类功能

--- application/Lib/Action/Admin/CategoryAction.class.php
+++ application/Lib/Action/Admin/CategoryAction.class.php
@@ -1,33 +1,26 @@
 <?php
-/**
- * CategoryAction
- *
- * @uses AppAction
- * @version $Id$
- * @author Thor Jiang <jiangyuntao@gmail.com>
- */
 class CategoryAction extends AppAction {
     protected $offset = 15;
 
     public function index() {
-        $categoryView = D('CategoryView');
+        $category = D('Category');
 
         import('ORG.Util.Page');
 
-        $condition = "language='{$this->data['setting']['default_language']}'";
+        $condition = '';
         if (isset($_GET['q'])) {
-            $condition .= " && (title LIKE '%{$_GET['q']}%' || content LIKE '%{$_GET['q']}%')";
+            $condition .= "name LIKE '%{$_GET['q']}%'";
             $this->data['q'] = $_GET['q'];
         }
 
-        $category_count = $categoryView->where($condition)->count('c.id');
+        $category_count = $category->where($condition)->count('id');
         $page = new Page($category_count, $this->offset);
         if (isset($_GET['q'])) {
             $page->parameter = 'q=' . urlencode($_GET['q']);
         }
 
-        $this->data['list'] = $categoryView->where($condition)
-            ->order('id desc')
+        $this->data['list'] = $category->where($condition)
+            ->order('sortorder desc, id desc')
             ->limit($page->firstRow . ',' . $page->listRows)
             ->select();
 
@@ -41,43 +34,18 @@ class CategoryAction extends AppAction {
         $category = D('Category');
 
         if ($this->isPost()) {
-            // 主表添加信息
             if ($category->create() && $id = $category->add()) {
-                // 处理 tag
-                if ($_POST['tag']) {
-                    $tag = D('Tag');
-                    $tag->process($_POST['tag'], $id, 'category');
-                }
-
-                // 添加不同语言内容
-                foreach ($this->data['language'] as $lang) {
-                    $categoryLocal = D('CategoryLocal');
-
-                    $categoryLocal->category_id = $id;
-                    $categoryLocal->language = $lang['language'];
-                    $categoryLocal->title = $_POST['title'][$lang['language']];
-                    $categoryLocal->keywords = $_POST['keywords'][$lang['language']];
-                    $categoryLocal->description = $_POST['description'][$lang['language']];
-                    $categoryLocal->content = $_POST['content'][$lang['language']];
-                    $categoryLocal->created = $categoryLocal->modified = time();
-
-                    if (!$categoryLocal->add()) {
-                        // 如果某一语言内容添加失败，删除之前添加的主表信息和其他语言内容
-                        $category->where("id='{$id}'")->delete();
-                        $categoryLocal->where("category_id='{$id}'")->delete();
-
-                        $this->assign('waitSecond', 2);
-                        $this->assign('jumpUrl', U('category/create'));
-                        return $this->error('创建失败！');
-                    }
-                }
+                // 根据 id 更新 sortorder
+                $category->find($id);
+                $category->sortorder = $id;
+                $category->save();
 
                 $this->assign('waitSecond', 2);
-                $this->assign('jumpUrl', U('category/index'));
+                $this->assign('jumpUrl', U('/admin/category/index'));
                 return $this->success('创建成功');
             } else {
                 $this->assign('waitSecond', 2);
-                $this->assign('jumpUrl', U('category/create'));
+                $this->assign('jumpUrl', U('/admin/category/create'));
                 return $this->error('创建失败');
             }
         }
@@ -89,57 +57,17 @@ class CategoryAction extends AppAction {
     public function modify() {
         $category = D('Category');
 
-        $this->data['category'] = $category->find($_GET['id']);
-
         if ($this->isPost()) {
-            if (!isset($_POST['recommended']) || !$_POST['recommended']) {
-                $_POST['recommended'] = '0';
-            }
-
-            // 如果上传了新图片，删除原图片
-            if ($this->data['category']['picture'] != $_POST['picture']) {
-                @unlink(realpath(__ROOT__) . $this->data['category']['picture']);
-            }
-
-            // 修改主表信息
             if ($category->create() && $category->save()) {
-                // 处理 tag
-                if ($_POST['tag']) {
-                    $tag = D('Tag');
-                    $tag->process($_POST['tag'], $_GET['id'], 'category');
-                }
-
-                // 修改不同语言内容
-                foreach ($this->data['language'] as $lang) {
-                    $categoryLocal = D('CategoryLocal');
-
-                    $categoryLocal->where("category_id='{$_GET['id']}' && language='{$lang['language']}'")->find();
-                    $categoryLocal->title = $_POST['title'][$lang['language']];
-                    $categoryLocal->keywords = $_POST['keywords'][$lang['language']];
-                    $categoryLocal->description = $_POST['description'][$lang['language']];
-                    $categoryLocal->content = $_POST['content'][$lang['language']];
-                    $categoryLocal->modified = time();
-
-                    if (!$categoryLocal->save()) {
-                        $this->assign('waitSecond', 2);
-                        $this->assign('jumpUrl', U('category/modify', 'id=' . $_GET['id']));
-                        return $this->error('修改失败！'. $categoryLocal->getError());
-                    }
-                }
-
                 $this->assign('waitSecond', 2);
-                $this->assign('jumpUrl', U('category/modify', 'id=' . $_GET['id']));
+                $this->assign('jumpUrl', U('/admin/category/modify', 'id=' . $_GET['id']));
                 return $this->success('修改成功');
             } else {
                 return $this->error('修改失败');
             }
         }
 
-        $categoryLocal = D('CategoryLocal');
-        $locals = $categoryLocal->where("category_id='{$_GET['id']}'")->select();
-        foreach ($locals as $v) {
-            $this->data['category']['language'][$v['language']] = $v;
-        }
+        $this->data['category'] = $category->find($_GET['id']);
 
         $this->assign($this->data);
         $this->display();
@@ -147,52 +75,49 @@ class CategoryAction extends AppAction {
 
     public function remove() {
         $category = D('Category');
-        $category->find($_GET['id']);
 
-        $categoryLocal = D('CategoryLocal');
-
-        if ($category->delete() && $categoryLocal->where("category_id='{$_GET['id']}'")->delete()) {
-            @unlink(realpath(__ROOT__) . $category->picture);
+        if ($category->delete($_GET['id'])) {
             $this->assign('waitSecond', 2);
-            $this->assign('jumpUrl', U('category/index'));
+            $this->assign('jumpUrl', U('/admin/category/index'));
             return $this->success('删除成功');
         } else {
             $this->assign('waitSecond', 2);
-            $this->assign('jumpUrl', U('category/index', $_SESSION['listpage']));
+            $this->assign('jumpUrl', U('/admin/category/index', $_SESSION['listpage']));
             return $this->error('删除失败' . $category->getError());
         }
     }
 
-    public function picture_upload() {
-        if (!empty($_FILES)) {
-            $tmp_file = $_FILES['Filedata']['tmp_name'];
-            $ext = strtolower(substr(strrchr($_FILES['Filedata']['name'], '.'), 1));
-            $target_path = $_SERVER['DOCUMENT_ROOT'] . $_REQUEST['folder'] . '/' . date('Ym/d/');
-            mkdir($target_path, 0777, true);
-            $target_file =  str_replace('//', '/', $target_path) . md5($_FILES['Filedata']['name'] . microtime(true)) . '.' . $ext;
-            $target_file_big =  str_replace('//', '/', $target_path) . md5($_FILES['Filedata']['name'] . 'big' . microtime(true)) . '.' . $ext;
+    public function up() {
+        $category = D('Category');
 
-            // 后缀检查
-            $allowed_ext = array('gif', 'bmp', 'jpg', 'jpeg', 'png');
-            if (!in_array($ext, $allowed_ext)) {
-                $this->ajaxReturn('', '请上传图片文件', 0);
-            }
+        $current = $category->find($_GET['id']);
+        $upper = $category->where("sortorder>'{$current['sortorder']}'")->order('sortorder asc, id asc')->limit(1)->find();
 
-            move_uploaded_file($tmp_file, $target_file);
-
-            // 缩放图片
-            vendor('WideImage.WideImage');
-            WideImage::load($target_file)
-                ->resize($this->data['setting']['category_big_picture_width'], $this->data['setting']['category_big_picture_height'])
-                ->saveToFile($target_file_big);
-            WideImage::load($target_file)
-                ->resize($this->data['setting']['category_picture_width'], $this->data['setting']['category_picture_height'])
-                ->saveToFile($target_file);
-
-            $this->ajaxReturn(array(
-                'picture' => str_replace($_SERVER['DOCUMENT_ROOT'], '', $target_file),
-                'big_picture' => str_replace($_SERVER['DOCUMENT_ROOT'], '', $target_file_big)
-            ), '', 1);
+        if ($upper) {
+            $tmp = $current['sortorder'];
+            $current['sortorder'] = $upper['sortorder'];
+            $upper['sortorder'] = $tmp;
+            $category->save($upper);
+            $category->save($current);
         }
+
+        redirect(U('/admin/category/index', $_SESSION['listpage']));
+    }
+
+    public function down() {
+        $category = D('Category');
+
+        $current = $category->find($_GET['id']);
+        $upper = $category->where("sortorder<'{$current['sortorder']}'")->order('sortorder desc, id desc')->limit(1)->find();
+
+        if ($upper) {
+            $tmp = $current['sortorder'];
+            $current['sortorder'] = $upper['sortorder'];
+            $upper['sortorder'] = $tmp;
+            $category->save($upper);
+            $category->save($current);
+        }
+
+        redirect(U('/admin/category/index', $_SESSION['listpage']));
     }
 }
